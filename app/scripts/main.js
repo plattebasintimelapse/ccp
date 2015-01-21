9033ca7fdd02a3e5596676613e2fa1715c96d1a6
Steven Speicher <stevenspeicher@gmail.com>
2015-01-21 11:13:27 -0600

    fix vig click, start sidebar p.3

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -15,181 +15,47 @@ function centerPosition(o) {
     o.css("margin-left", -(o.width() / 2) );
 }
 
+// listenVideo('snow');
 
-var map;
-var ccpGeoJSON;
-// var vigs = [];
-// var vigs = [
-//     {
-//         "title": "A Title Goes Here",
-//         "lat": "41",
-//         "long":"-99",
-//         "direction":"up",
-//         "html_src":"test1",
-//         "meta":"Some meta information",
-//         "section":"1",
-//         "order":"1"
-//     },{
-//         "title": "A Title Goes Here Two",
-//         "lat": "41.1",
-//         "long":"-99.5",
-//         "direction":"up",
-//         "html_src":"test2",
-//         "meta":"Some meta information",
-//         "section":"1",
-//         "order":"2"
-//     }
-// ]
+function listenVideo(id){
+    // console.log(id + ' video ready');
+    var $iframe = $('#' + id)[0];
+    var player = $f($iframe);
 
+    var $inline_btn = $('.inline-video-btn i');
+    var $vid_containder = $('#' + id + '-video');
 
+    player.addEvent('ready', function() {
+        player.addEvent('play', play);
+        player.addEvent('finish', onFinish);
+        player.addEvent('pause', pause);
 
-var ccpOwners = [
-    {
-        'name': 'Audubon',
-        'ownership': 'Audubon',
-        'color': 'rgb(141,211,199)',
-        'filter': true
-    }, {
-        'name': 'CNPPID',
-        'ownership': 'CNPPID',
-        'color': '#FFDF30',
-        'filter': true
-    }, {
-        'name': 'Ducks Unlimited',
-        'ownership': 'Ducks Unlimited',
-        'color': 'rgb(190,186,218)',
-        'filter': true
-    }, {
-        'name': 'Nebraska Game & Parks',
-        'color': 'rgb(251,128,114)',
-        'ownership': 'NGPC',
-        'filter': true
-    }, {
-        'name': 'NPPD',
-        'ownership': 'NPPD',
-        'color': 'rgb(128,177,211)',
-        'filter': true
-    }, {
-        'name': 'PRRIP',
-        'ownership': 'PRRIP',
-        'color': 'rgb(253,180,98)',
-        'filter': true
-    }, {
-        'name': 'The Crane Trust',
-        'ownership': 'PRWCT',
-        'color': 'rgb(179,222,105)',
-        'filter': true
-    }, {
-        'name': 'The Nature Conservancy',
-        'ownership': 'TNC',
-        'color': 'rgb(252,205,229)',
-        'filter': true
-    }, {
-        'name': 'USFWS',
-        'ownership': 'USFWS',
-        'color': 'rgb(217,217,217)',
-        'filter': true
-    }
-];
-
-var ccpLength = ccpOwners.length;
-
-
-function makeMap() {
-
-	map = L.map('map', {
-		center: [40.8,-99.1],
-		zoom: 10
-	})
-
-	var tonerUrl = "http://{S}tile.stamen.com/toner/{Z}/{X}/{Y}.png";
-
-	var url = tonerUrl.replace(/({[A-Z]})/g, function(s) {
-	    return s.toLowerCase();
-	});
-
-
-	var basemap = L.tileLayer(url, {
-	    subdomains: ['','a.','b.','c.','d.'],
-	    minZoom: 6,
-	    maxZoom: 14,
-	    type: 'png',
-	    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>'
-	});
-
-	basemap.addTo(map);
-
-    getVigs();
-    mapData();
-}
-
-function getVigs() {
-    var vigs = [];
-    $.getJSON( "../../content/vigs.json", function( data ) {
-        console.log(data);
-        vigs = data;
-    }).done(function() {
-        mapVigs(vigs);
-    });
-}
-
-function mapVigs(vigs) {
-    for (var i=0; i < vigs.length; i++) {
-
-        var mlat = parseFloat(vigs[i].lat);
-        var mlong = parseFloat(vigs[i].long);
-
-        var vig = new Vignette( [mlat ,mlong], {
-            title: vigs[i].title,
-            order: vigs[i].order,
-            direction: vigs[i].direction,
-            html_src: vigs[i].html_src,
-
-        } ).addTo(map).on('click', function() {
-            vig.click();
+        var playing = false;
+        $('#play-' + id).click(function() {
+            if (playing) {
+                player.api('unload');
+                playing = false;
+            } else {
+                player.api('play');
+                playing = true;
+            }
         });
-
-        vig.info();
-    }
-}
-
-function mapData() {
-
-    function bindLabel (feature, layer) {
-       // layer.bindPopup('Name: <b>' + feature.properties.NAME + '</b><br/>Owner: <b>' + feature.properties.OWNERSHIP + '</b><br/>Purchased: <b>' + feature.properties.PRRIPTract);
-       // layer.bindLabel('Name: <b>' + feature.properties.NAME + '</b><br/>Owner: <b>' + feature.properties.OWNERSHIP + '</b><br/>Purchased: <b>' + feature.properties.PRRIPTract, { noHide: true, className: 'layer-label' });
-    }
-
-    $.getJSON('../data/big-bend-conservation.json', function(data) {
-        ccpGeoJSON = L.geoJson(data, {
-            style: function(feature){
-
-                var styles = {
-                    fillOpacity: .9,
-                    weight: 1,
-                    color: 'white'
-                }
-
-                switch (feature.properties.OWNERSHIP) {
-                    case "Audubon":  styles.fillColor=ccpOwners[0].color; break;
-                    case "CNPPID":  styles.fillColor=ccpOwners[1].color; break;
-                    case "Ducks Unlimited":  styles.fillColor=ccpOwners[2].color; break;
-                    case "NGPC":  styles.fillColor=ccpOwners[3].color; break;
-                    case "NPPD":  styles.fillColor=ccpOwners[4].color; break;
-                    case "PRRIP":  styles.fillColor=ccpOwners[5].color; break;
-                    case "PRWCT":  styles.fillColor=ccpOwners[6].color; break;
-                    case "TNC":  styles.fillColor=ccpOwners[7].color; break;
-                    case "USFWS":  styles.fillColor=ccpOwners[8].color; break;
-                }
-
-                return styles;
-            },
-            onEachFeature: bindLabel
-
-        }).addTo(map);
-
     });
 
+    function play(id) {
+        $vid_containder.toggleClass('playing-inline-video');
+        $inline_btn.addClass('fa-pause').removeClass('fa-play');
+    }
+
+    function pause(id) {
+        $vid_containder.toggleClass('playing-inline-video');
+        $inline_btn.addClass('fa-play').removeClass('fa-pause');
+    }
+
+    function onFinish(id) {
+        $vid_containder.removeClass('playing-inline-video');
+        $inline_btn.addClass('fa-play').removeClass('fa-pause');
+    }
 }
 
 $( document ).ready(function() {
