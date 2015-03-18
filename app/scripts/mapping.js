67a6f47067abdec53f8584d4117b90fc8774127f
Steven Speicher <stevenspeicher@gmail.com>
2015-03-18 10:36:42 -0500

    add multiple popup size support

--- app/scripts/mapping.js
+++ app/scripts/mapping.js
@@ -155,18 +155,26 @@ function mapVigs(vigs) {
             }
         }
 
-        for (var type in types) { types_classes += '<i class="fa fa-' + types[type] + ' fa-lg"></i>'; }
+        for (var type in types) { types_classes += '<i class="fa fa-' + types[type] + ' fa"></i>'; }
+
+        if ( vigs[i].size == 'square' ) {
+            vig_content = '<div class="row"><div class="col col-xs-12"><img src="../images/thumbnail/' + vigs[i].image + '"/></div></div>';
+        } else if ( vigs[i].size == 'full' ) {
+            vig_content = '<div class="row"><div class="col col-xs-6"><img src="../images/thumbnail/' + vigs[i].image + '"/></div><div class="col col-xs-6"><h5 class="title">' + vigs[i].title + '</h5><div class="vig-types">' + types_classes + '</div></div></div></div>';
+        } else if ( vigs[i].size == 'video' ) {
+            vig_content = '<div class="row"><div class="col col-xs-12"><img src="../images/thumbnail/' + vigs[i].image + '"/><h5 class="atop-icon"><i class="fa fa-play fa-3x"></i></h5></div></div>';
+        } else if ( vigs[i].size == 'map' ) {
+            vig_content = '<div class="row"><div class="col col-xs-12"><img src="../images/thumbnail/' + vigs[i].image + '"/><h5 class="atop-icon"><i class="fa fa-map-marker fa-3x"></i></h5></div></div>';
+        }
 
         if ( vigs[i].new ) {
-            vig_content = '<div class="new-wrapper"><div class="new-ribbon-wrapper"><div class="new-ribbon">NEW</div></div><div class="row"><div class="col-xs-6"><img src="../images/thumbnail/' + vigs[i].image + '"/></div><div class="col-xs-6"><h5 class="title">' + vigs[i].title + '</h5><div class="vig-types">' + types_classes + '</div></div></div></div>';
-        } else {
-            vig_content = '<div class="row"><div class="col-xs-6"><img src="../images/thumbnail/' + vigs[i].image + '"/></div><div class="col-xs-6"><h5 class="title">' + vigs[i].title + '</h5><div class="vig-types">' + types_classes + '</div></div></div></div>';
+            vig_content = '<div class="new-wrapper"><div class="new-ribbon-wrapper"><div class="new-ribbon">NEW</div></div>' + vig_content;
         }
 
         var vig = new Vignette({
             page: vigs[i].page,
             new: vigs[i].new,
-            className: vigs[i].page + ' vig-popup'
+            className: vigs[i].page + ' vig-popup ' + vigs[i].size
         }).setContent(vig_content).setLatLng([mlat ,mlong]);
 
         map.addLayer(vig);
@@ -182,7 +190,7 @@ $('#map').on('click', '.vig-popup', function() {
 
     // strip away unnecessary classes
     for (var i=0; i<c.length; i++) {
-        if ( c[i] != 'leaflet-popup' && c[i] != 'vig-popup' && c[i] != 'leaflet-zoom-animated') {
+        if ( c[i] != 'leaflet-popup' && c[i] != 'vig-popup' && c[i] != 'leaflet-zoom-animated' && c[i] != 'square' && c[i] != 'full' && c[i] != 'video' && c[i] != 'map') {
             f = c[i];
         }
     }
