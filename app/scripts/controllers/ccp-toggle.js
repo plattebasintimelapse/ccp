2175110437b590f3389c58bddbf2cbdd8f411c1a
Steven Speicher <stevenspeicher@gmail.com>
2015-03-24 11:28:25 -0500

    prep for p.3 launch, ccp mapping fixes

--- app/scripts/controllers/ccp-toggle.js
+++ app/scripts/controllers/ccp-toggle.js
@@ -1,37 +1,43 @@
 
-	var $ccp_land = $('#ccp-land');
-	var $ccp_toggle = $('.toggle-btn');
+var $ccp_land = $('#ccp-land');
+var $ccp_toggle = $('.toggle-btn');
 
-		
-			$ccp_land.hover(function() {
-				if ( !$ccp_land.hasClass('open') ) {
-					$ccp_land.animate({
-						right: '-230px'
-					}, 300, 'linear' );
-				}
-			},function() {
-				if ( !$ccp_land.hasClass('open') ) {
-					setTimeout(function() {
-						$ccp_land.animate({
-							right: '-240px'
-						}, 300, 'linear' );
-					}, 300);
-						
-				}
-			});
 
+	
+$ccp_land.hover(function() {
+	if ( !$ccp_land.hasClass('open') ) {
+		$ccp_land.animate({
+			right: '-230px'
+		}, 300, 'linear' );
+	}
+},function() {
+	if ( !$ccp_land.hasClass('open') ) {
+		setTimeout(function() {
+			$ccp_land.animate({
+				right: '-240px'
+			}, 300, 'linear' );
+		}, 300);
+	}
+});
+
+var ccpToggle = function() {
 	$ccp_toggle.click(function() {
 
 		if ( $ccp_land.hasClass('open') ) {
 			$ccp_land.animate({
 				right: '-240px'
 			}, 300, 'linear' );
+			$ccp_land.find('.toggle-btn i').addClass('fa-caret-up').removeClass('fa-caret-down');
 		} else {
 			$ccp_land.animate({
 				right: '20px'
 			}, 300, 'linear' );
+			$ccp_land.find('.toggle-btn i').removeClass('fa-caret-up').addClass('fa-caret-down');
 		}
 
 		$ccp_land.toggleClass('open');
 	});
+}
+
+ccpToggle();
 
